2a39162a84e5827903fb9e70c44f90b7293815ac
Omur Ozbahceliler <omur@rootifera.org>
2024-03-12 17:04:55 +0000

    setup.py updates

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+
+def read_requirements():
+    try:
+        with open('requirements.txt') as f:
+            return f.read().splitlines()
+    except FileNotFoundError:
+        return []
+
 
 setup(
     name='mancaveman',
-    version='1.0.0',
+    version='0.2.0',
     author='Omur Ozbahceliler',
-    packages=find_packages(),
-    install_requires=requirements,
+    packages=find_packages(exclude=('tests*', 'docs')),
+    install_requires=read_requirements(),
+    python_requires='==3.10.*',
+    description='Mancave manager',
+    url='https://github.com/rootifera/mancaveman-postgresql',
 )
